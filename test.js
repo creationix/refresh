44348d4cb7af0abbeb7ab38096f13daaefb60efd
Tim Caswell <tim@creationix.com>
2010-02-23 15:26:06 -0600

    Start to decipher the crazy xml required to speak webdav.

--- test.js
+++ test.js
@@ -9,6 +9,40 @@ var tree = {
 var http = require('http');
 process.mixin(require('sys'));
 
+// A super dumb xml parser that works enough for our needs.
+function get_props(xml) {
+  var props = xml.match(/<D:\w+\/>/g);
+  props = props.map(function (prop) {
+    return prop.match(/<D:(\w+)\/>/)[1];
+  })
+  puts(xml);
+  return props;
+}
+
+function propfind(props, callback) {
+  
+  <?xml version="1.0" encoding="UTF-8"?>
+  <D:multistatus xmlns:D="DAV:">
+    <D:response>
+      <D:href>http://127.0.0.1:8080/</D:href>
+      <D:propstat>
+        <D:prop>
+          <D:resourcetype><D:collection/></D:resourcetype>
+          <D:getlastmodified>Tue, 23 Feb 2010 21:02:41 GMT</D:getlastmodified>
+        </D:prop>
+        <D:status>HTTP/1.1 200 OK</D:status>
+      </D:propstat>
+      <D:propstat>
+        <D:prop>
+          <D:getcontentlength/>
+        </D:prop>
+        <D:status>HTTP/1.1 404 Not Found</D:status>
+      </D:propstat>
+    </D:response>
+  </D:multistatus>
+  
+}
+
 http.createServer(function (req, res) {
   
   function get_body(callback) {
@@ -34,17 +68,43 @@ http.createServer(function (req, res) {
       // Sample body captured from WebDAVFS/1.8 (01808000) Darwin/10.2.0 (x86_64)
         // <?xml version="1.0" encoding="utf-8"?>
         // <D:propfind xmlns:D="DAV:">
-        // <D:prop>
-        // <D:getlastmodified/>
-        // <D:getcontentlength/>
-        // <D:resourcetype/>
-        // </D:prop>
+        //   <D:prop>
+        //     <D:getlastmodified/>
+        //     <D:getcontentlength/>
+        //     <D:resourcetype/>
+        //   </D:prop>
         // </D:propfind>
       // Sample response from sling
+        // <?xml version="1.0" encoding="UTF-8"?>
+        // <D:multistatus xmlns:D="DAV:">
+        //   <D:response>
+        //     <D:href>http://127.0.0.1:8080/</D:href>
+        //     <D:propstat>
+        //       <D:prop>
+        //         <D:resourcetype><D:collection/></D:resourcetype>
+        //         <D:getlastmodified>Tue, 23 Feb 2010 21:02:41 GMT</D:getlastmodified>
+        //       </D:prop>
+        //       <D:status>HTTP/1.1 200 OK</D:status>
+        //     </D:propstat>
+        //     <D:propstat>
+        //       <D:prop>
+        //         <D:getcontentlength/>
+        //       </D:prop>
+        //       <D:status>HTTP/1.1 404 Not Found</D:status>
+        //     </D:propstat>
+        //   </D:response>
+        // </D:multistatus>
+      
       get_body(function (body) {
-        p(req);
-        puts(body);
-        res.writeHeader(200, {'Content-Type': 'application/json'});
+        var json = get_props(body)
+        p(json);
+        <?xml version="1.0" encoding="UTF-8"?><D:multistatus xmlns:D="DAV:"><D:response><D:href>http://127.0.0.1:8080/</D:href><D:propstat><D:prop><D:resourcetype><D:collection/></D:resourcetype><D:getlastmodified>Tue, 23 Feb 2010 21:02:41 GMT</D:getlastmodified></D:prop><D:status>HTTP/1.1 200 OK</D:status></D:propstat><D:propstat><D:prop><D:getcontentlength/></D:prop><D:status>HTTP/1.1 404 Not Found</D:status></D:propstat></D:response></D:multistatus>
+        
+        res.writeHeader(200, {
+          "Content-Type": "text/xml; charset=utf-8",
+          "Content-Length": xml.length
+        });
+        
         res.write(inspect(req));
         res.close();
       });
